208726433f2e03df33074b216ca3aae7d1c8bea5
Hunter Clezie <clezhunt@gmail.com>
2026-02-11 21:32:57 -0700

    init

--- ConvertAndValidate/Program.cs
+++ ConvertAndValidate/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+
+            do
+            {
+
+            } while (userInput != "Q" || userInput != "q");
+
+            //pause
+            Console.Read();
         }
     }
 }
